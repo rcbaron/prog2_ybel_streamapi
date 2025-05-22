e3d39fa3e39a2bd413213e26c58ff77c0e56c346
rcbaron <r.baron@live.de>
2025-05-22 16:36:16 +0200

    [task:ii] replaced logic from method 'ifmCPS' with stream-api logic.

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -2,6 +2,7 @@ package streamapi;
 
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -51,18 +52,11 @@ public class Main {
      * @param studentList List of students
      * @return Set of credit points of all IFM students
      */
-    public static Set<Integer> ifmCps(List<Student> studentList) {
-        // TODO
-        Set<Integer> result = new HashSet<>();
-        Integer i = 0;
-        for (Student v : studentList) {
-            if (v.isIFM()) {
-                i = v.cps();
-                result.add(i);
-            }
-        }
 
-        return result;
+    // returns the cps of all students with the attribute IFM in a Set,
+    // Sets don't have duplicate values.
+    public static Set<Integer> ifmCps(List<Student> studentList) {
+       return studentList.stream().filter(Student::isIFM).map(Student::cps).collect(Collectors.toSet());
     }
 
     /**
